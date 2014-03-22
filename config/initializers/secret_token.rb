0806f580bbee0d3f7e9afd2cd3bba34208938b4d
Koga Osamu <osak.63@gmail.com>
2014-03-22 15:51:55 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cherry::Application.config.secret_key_base = '402c1977b7ae5e6f89799f02f598a10319386b529984fdf079c472015a8786acaf8623c2edadd47c90f1192e4c06e226feea2becc8ba7da6f7603a0bd7fe3933'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cherry::Application.config.session_store :cookie_store, key: '_Cherry_session'
