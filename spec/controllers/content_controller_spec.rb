d286ca1686e4bf7a96205ce7c918dcd9db85cd8c
Koga Osamu <osak.63@gmail.com>
2014-03-23 02:26:38 +0900

    Select appropriate renderer by kind

--- spec/controllers/content_controller_spec.rb
+++ spec/controllers/content_controller_spec.rb
@@ -1,34 +1,51 @@
 require 'spec_helper'
 
 describe ContentsController do
+  let(:text_file) { fixture_file_upload("files/test.txt", "text/plain") }
+  let(:image_file) { fixture_file_upload("files/test.jpg", "image/jpeg") }
+
   context('#upload') do
     let(:name){ "sakuratrick" }
 
     it "create new content from uploaded file" do
-      file = fixture_file_upload("files/test.txt", "text/plain")
       expect do
-        post :upload, content: {name: name, file: file}
+        post :upload, content: {name: name, file: text_file}
       end.to change{Content.count}.by 1
       expect(response).to be_redirect
       expect(Content.last.name).to eq(name)
-      expect(Content.last.size).to eq(file.size)
+      expect(Content.last.size).to eq(text_file.size)
       expect(Content.last.kind).to eq(Content::Kind::Text)
     end
 
     it "create new content from uploaded file with default name" do
-      file = fixture_file_upload("files/test.txt", "text/plain")
       expect do
-        post :upload, content: {file: file}
+        post :upload, content: {file: text_file}
       end.to change{Content.count}.by 1
       expect(Content.last.name).to eq("test.txt")
     end
 
     it "distinguish image from other kind of files" do
-      file = fixture_file_upload("files/test.jpg", "image/jpeg")
       expect do
-        post :upload, content: {file: file}
+        post :upload, content: {file: image_file}
       end.to change{Content.count}.by 1
       expect(Content.last.kind).to eq(Content::Kind::Image)
+    end
+  end
+
+  context("#show") do
+    let(:text) { FactoryGirl.create(:content, file: text_file) }
+    let(:image) { FactoryGirl.create(:content, file: image_file) }
+
+    render_views
+    it "render text template for text file" do
+      get :show, id: text.id
+      assert_select 'blockquote'
+    end
+
+    render_views
+    it "render image template for image file" do
+      get :show, id: image.id
+      assert_select 'img'
     end
   end
 end
