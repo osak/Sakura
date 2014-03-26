6dbda8027c21c43eab3d1caa96122ffc099f09b7
Koga Osamu <osak.63@gmail.com>
2014-03-27 02:05:33 +0900

    Implement Login/Logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   layout 'application'
+
+  before_action :set_current_user
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
