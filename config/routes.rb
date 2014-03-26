31751e81926d4a8cc173bc183c57b20f0903fc29
Koga Osamu <osak.63@gmail.com>
2014-03-27 01:50:41 +0900

    Fix route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Cherry::Application.routes.draw do
       post 'upload', action: :upload
     end
   end
-  match '/auth/:provider/callback' => 'sessions#callback', via: [:get, :post], as: :login
+  get '/auth/:provider', to: 'sessions#dummy', as: :login
+  match '/auth/:provider/callback' => 'sessions#callback', via: [:get, :post]
   match '/logout' => 'sessions#destroy', as: :logout, via: [:get]
 
   root 'home#index'
